bb5e475f85b800d0ba8b8893df399004912f41ef
Sebastian <sebastiandelgadobruno@gmail.com>
2024-03-06 17:07:16 +0100

    Agregamos Herencia e iniciamos el Admin de Django

--- vivoverde/admin.py
+++ vivoverde/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Receta, Usuario, Blog
+
+admin.site.register(Receta)
+admin.site.register(Usuario)
+admin.site.register(Blog)
+
