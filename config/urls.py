3ac573fe76d071d63153709a2130ab3aa227514f
Sebastian <sebastiandelgadobruno@gmail.com>
2024-03-07 14:53:34 +0100

    Agregamos login, registro y logout

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vivoverde/', include("vivoverde.urls")),
+    path('users/', include('users.urls'))
 ]
